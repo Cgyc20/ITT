135705615bda655bbb0385b01a1a569d6280b080
Cgyc20 <cgyc20@bath.ac.uk>
2026-01-27 17:13:33 +0000

    First commit

---
+++ entrainment.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+dt = 0.1
+total_time = 100
+
+v_0 = 0.1
+p_crit = 0.1
+alpha_p = 1
+beta_c = 1
+beta_p = 1
+gamma = 1
+n = 2
+
+p_0 = 0.1
+c_0 = 0.2
+
+def light(t):
+    # 1 for first 12 hours of each 24h cycle, else 0
+    return 1.0 if (t % 24) < 12 else 0.0
+
+def deriv(p, c, t):
+    L = light(t)
+    dcdt = v_0 / (1.0 + (p / p_crit)**n) - beta_c * c
+    dpdt = alpha_p * c - beta_p * p - gamma * L
+    return dcdt, dpdt
+
+def fwd_euler(p0, c0, dt, total_time):
+    t = np.arange(0, total_time + dt, dt)
+    p = np.zeros_like(t, dtype=float)
+    c = np.zeros_like(t, dtype=float)
+
+    p[0] = p0
+    c[0] = c0
+
+    for i in range(len(t) - 1):
+        dcdt, dpdt = deriv(p[i], c[i], t[i])
+        c[i+1] = c[i] + dt * dcdt
+        p[i+1] = p[i] + dt * dpdt
+
+    L = np.array([light(tt) for tt in t])
+    return t, p, c, L
+
+t, p, c, L = fwd_euler(p_0, c_0, dt, total_time)
+
+plt.figure()
+plt.plot(t, c, label="C(t)")
+plt.plot(t, p, label="P(t)")
+
+plt.xlabel("Time (h)")
+plt.legend()
+plt.show()
